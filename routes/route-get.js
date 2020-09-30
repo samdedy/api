b233f9503c5561c3623ce9d1ab073f9f1024ca18
samga <samgalaxy111111@gmail.com>
2020-10-01 06:26:47 +0700

    post api, input data to mysql

--- routes/route-get.js
+++ routes/route-get.js
@@ -14,4 +14,24 @@ exports.getAllData = router.get("/api/all", (req,res) => {
     })
 });
 
+exports.postData = router.post("/api/postData", (req, res) => {
+    const { nama, jabatan, usia, tempat_lahir, tanggal_lahir } = req.body;
+    let arr_input = [ nama, jabatan, usia, tempat_lahir, tanggal_lahir ];
+    let object_input = { nama, jabatan, usia, tempat_lahir, tanggal_lahir };
+
+    // Validasi parameter kurung, dan parameter diisi kosong
+    if (arr_input.includes("") || arr_input.includes(undefined)){
+        res.send("Ada inputan yang belum terisi");
+    } else {
+        mySQL.query("INSERT INTO karyawan SET ?", object_input, (err, result, fields) => {
+            if (err) console.log(err);
+            res.send({
+                code: 200,
+                message: "Add data karyawan berhasil",
+                result: JSON.stringify(result)
+            });
+        })
+    }
+});
+
 module.exports = router;
